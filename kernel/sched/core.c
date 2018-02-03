7edf30a666227af39d4859f98a82232592a0f234
acuicultor <ramoncuenca4@gmail.com>
2018-02-03 18:53:30 +0100

    Revert "sched: Trying to reduce power usage"
    This reverts commit 0b682970ab0df2d9953f61d9addd3451d9928665.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5,8 +5,6 @@
  *
  *  Copyright (C) 1991-2002  Linus Torvalds
  *
- *  Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
- *
  *  1996-12-23  Modified by Dave Grothe to fix bugs in semaphores and
  *		make semaphores SMP safe
  *  1998-11-19	Implemented schedule_timeout() and related stuff
@@ -94,8 +92,6 @@
 #include "sched.h"
 #include "../workqueue_internal.h"
 #include "../smpboot.h"
-#include <asm-generic/relaxed.h>
-#include <asm-generic/processor.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/sched.h>
@@ -1622,7 +1618,6 @@ static inline int got_boost_kick(void)
 
 	return test_bit(BOOST_KICK, &rq->hmp_flags);
 }
-
 
 static inline void clear_boost_kick(int cpu)
 {
@@ -4590,10 +4585,9 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		 * is actually now running somewhere else!
 		 */
 		while (task_running(rq, p)) {
-			if (match_state && unlikely(cpu_relaxed_read_long
-				(&(p->state)) != match_state))
+			if (match_state && unlikely(p->state != match_state))
 				return 0;
-			cpu_read_relax();
+			cpu_relax();
 		}
 
 		/*
@@ -5088,8 +5082,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	 * If the owning (remote) cpu is still in the middle of schedule() with
 	 * this task as prev, wait until its done referencing the task.
 	 */
-	while (cpu_relaxed_read(&(p->on_cpu)))
-		cpu_read_relax();
+	while (p->on_cpu)
+		cpu_relax();
 	/*
 	 * Pairs with the smp_wmb() in finish_lock_switch().
 	 */
